44e740fc1d82e22008fe5a4ed32e7e0f68f85b68
Ashesh Kumar Singh <user501254@gmail.com>
2015-11-18 03:21:54 +0530

    NS2,Nam installation script

---
+++ Installation_NS2-Nam/Installation_NS2-Nam.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#first give execute permission to script
+# sudo chmod +x Installation_NS2-Nam.sh
+#then use execute script
+# ./Installation_NS2-Nam.sh
+
+sudo apt-get install -y ns2
+
+if [[ "$(uname -m)" = "x86_64" ]]; then
+	ARCH="amd64"
+else
+	ARCH="i386"
+fi
+
+CACHEDIR="/var/cache/ns2_nam";
+mkdir -p "$CACHEDIR"
+cd "$CACHEDIR"
+
+URL=https://raw.githubusercontent.com/user501254/BE_IT_7_WT/master/Installation_NS2-Nam/nam_1.15-10_$ARCH.deb
+FILE=${URL##*/}
+
+wget -c "$URL" -O "$FILE"
+
+if [[ ! -f "$FILE" ]]; then
+	exit 1
+fi
+
+sudo dpkg -i $FILE
+
+
+#check
+dpkg -l|grep ns2 
+nam
